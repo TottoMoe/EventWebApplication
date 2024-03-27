dff2bb161d1dd713845a217b0097ac2624672fba
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2024-03-26 17:39:29 -0700

    sixth commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -55,9 +55,10 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  if(eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-    
+  if (eventType === "user.created") {
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
+
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
@@ -73,8 +74,8 @@ export async function POST(req: Request) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
-        }
-      })
+        },
+      });
     }
 
     return NextResponse.json({ message: "OK", user: newUser });
@@ -84,9 +85,9 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      username: username!,
       firstName: first_name,
       lastName: last_name,
+      username: username!,
       photo: image_url,
     };
 
